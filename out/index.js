1ef55e6e5edc01db6b399612f07f73d6d6820197
Brian Di Palma <offler@gmail.com>
2014-07-14 23:34:08 +0100

    Use `minimist` for option parsing.

--- out/index.js
+++ out/index.js
@@ -10,14 +10,16 @@ var readFileSync = require('fs').readFileSync;
 var $__2 = require('recast'),
     parse = $__2.parse,
     print = $__2.print;
+var minimist = require('minimist');
 var flattenNamespace = require('./transforms/flatten').flattenNamespace;
-function compileFile($__4) {
-  var $__5 = $traceurRuntime.assertObject($__4),
-      fileLocation = $__5[0],
-      fullyQualifiedName = $__5[1];
+function compileFile(options) {
+  var args = minimist(options);
+  var fileLocation = args._[0];
+  var namespace = args.flatten;
   var filePath = join(process.cwd(), fileLocation);
   var fileContents = readFileSync(filePath);
   var ast = parse(fileContents);
-  flattenNamespace(ast.program);
+  flattenNamespace(ast.program, namespace);
   console.log(print(ast).code);
+  return print(ast).code;
 }
--- src/index.js
+++ src/index.js
@@ -5,18 +5,23 @@ import {
 	parse,
 	print
 } from 'recast';
+module minimist from 'minimist';
 
 import {flattenNamespace} from './transforms/flatten';
 
 /**
  * @param {Array} options - List of options for compiler.
  */
-export function compileFile([fileLocation, fullyQualifiedName]) {
+export function compileFile(options) {
+	var args = minimist(options);
+	var fileLocation = args._[0];
+	var namespace = args.flatten;
 	var filePath = join(process.cwd(), fileLocation);
 	var fileContents = readFileSync(filePath);
 	var ast = parse(fileContents);
 
-	flattenNamespace(ast.program);
+	flattenNamespace(ast.program, namespace);
 
 	console.log(print(ast).code);
+	return print(ast).code;
 }
