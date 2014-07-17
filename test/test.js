3293c1096af6847b5bcf0c9ec932062a90e24ecd
Brian Di Palma <offler@gmail.com>
2014-07-17 22:11:32 +0100

    Assert compiled test class is as expected.

--- test/test.js
+++ test/test.js
@@ -1,13 +1,19 @@
 'use strict';
 
-var globalCompiler = require("../");
+var fs = require('fs');
+var assert = require('assert');
+
+var globalCompiler = require('../');
 
 describe('GlobalCompiler flattening', function() {
 	it('should remove the class namespace.', function() {
+		var expectedResult = fs.readFileSync('test/flatten/SimpleClassFlattened.js', {encoding: 'utf-8'});
 		var code = globalCompiler.compileFile([
 			'--flatten',
 			'my.long.name.space.SimpleClass',
 			'test/flatten/SimpleClass.js'
 		]);
+
+		assert.equal(code, expectedResult);
 	})
 });
