c1758b4d240f1c50f2bfd017726cbefdb9f7f296
Brian Di Palma <offler@gmail.com>
2014-07-18 23:28:27 +0100

    Add CallExpressions to test files.

--- test/flatten/SimpleClass.js
+++ test/flatten/SimpleClass.js
@@ -1,5 +1,7 @@
 my.long.name.space.SimpleClass = function() {}
 
+my.extend(my.long.name.space.SimpleClass, my.long.name.space.SuperClass);
+
 my.long.name.space.SimpleClass.prototype.myMethod = function() {}
 
 my.long.name.space.SimpleClass.prototype.anotherMethod = function() {}
--- test/flatten/SimpleClassFlattened.js
+++ test/flatten/SimpleClassFlattened.js
@@ -1,3 +1,4 @@
 function SimpleClass() {}
+my.extend(SimpleClass, my.long.name.space.SuperClass);
 SimpleClass.prototype.myMethod = function() {}
 SimpleClass.prototype.anotherMethod = function() {}
