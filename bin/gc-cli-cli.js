3208ca62b0ffe535c14950865891da6c1c0ec1a2
Brian Di Palma <offler@gmail.com>
2014-07-26 23:35:26 +0100

    Initial commit.
    Generated by slush node-esnext generator.

---
+++ bin/gc-cli-cli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+"use strict";
+
+var gc-cli = require("../");
+
+//process.argv.slice(2)
+//process.argv - Array that contains the command line arguments.
---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var traceurRuntimePath = require("traceur").RUNTIME_PATH;
+var traceurRuntime = require(traceurRuntimePath);
+
+module.exports = require("./out/");
