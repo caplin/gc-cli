bd75f3238ca177dcf8bc11bb90531743db4e2941
Brian Di Palma <offler@gmail.com>
2014-08-22 08:16:56 +0100

    Modify where test resouce files are located.

--- test/flatten.js
+++ test/flatten.js
@@ -7,11 +7,11 @@ var globalCompiler = require('../');
 
 describe('GlobalCompiler flattening', function() {
 	it('should remove the class namespace.', function() {
-		var expectedResult = fs.readFileSync('test/flatten/SimpleClassFlattened.js', {encoding: 'utf-8'});
+		var expectedResult = fs.readFileSync('test/flatten/expected.js', {encoding: 'utf-8'});
 		var code = globalCompiler.compileFile([
 			'--flatten',
 			'my.long.name.space.SimpleClass',
-			'test/flatten/SimpleClass.js'
+			'test/flatten/given.js'
 		]);
 
 		assert.equal(code, expectedResult);
