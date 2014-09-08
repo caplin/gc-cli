8628643cfa22d67f2ee5bfd39140521ba74aede1
Brian Di Palma <offler@gmail.com>
2014-09-08 17:36:10 +0100

    Use `SystemJS` for flattening test.

--- test/flatten.js
+++ test/flatten.js
@@ -3,17 +3,35 @@
 var fs = require('fs');
 var assert = require('assert');
 
-var globalCompiler = require('../');
+var System = require('systemjs');
+var parse = require('recast').parse;
+var print = require('recast').print;
+var visit = require('ast-types').visit;
+
+var given = fs.readFileSync('test/flatten/given.js', {encoding: 'utf-8'});
+var expected = fs.readFileSync('test/flatten/expected.js', {encoding: 'utf-8'});
+var givenAst = parse(given);
 
 describe('Namespaced class flattening', function() {
-	it('should remove the class namespace.', function() {
-		var expectedResult = fs.readFileSync('test/flatten/expected.js', {encoding: 'utf-8'});
-		var code = globalCompiler.compileFile([
-			'--flatten',
-			'my.long.name.space.SimpleClass',
-			'test/flatten/given.js'
-		]);
-
-		assert.equal(code, expectedResult);
-	})
+	it('should remove the class namespace.', function(done) {
+		System.import('../index')
+			.then(shouldRemoveClassNamespace.bind(null, done))
+			.catch(function(error) {
+				done(error);
+			});
+	});
 });
+
+function shouldRemoveClassNamespace(done, flattenModule) {
+	//Given.
+	var namespacedClassVisitor = flattenModule.namespacedClassVisitor;
+	namespacedClassVisitor.initialize('my.long.name.space.SimpleClass');
+
+	//When.
+	visit(givenAst, namespacedClassVisitor);
+
+	//Then.
+	assert.equal(print(givenAst).code, expected);
+
+	done();
+}
