8cdd4adf9fc51a904327872f8b43c6ea8e02e3b9
Brian Di Palma <offler@gmail.com>
2014-09-10 09:22:51 +0100

    Add UT that uses ES6 module loader.

--- test/test.js
+++ test/test.js
@@ -3,16 +3,43 @@
 var fs = require('fs');
 var assert = require('assert');
 
-var globalCompilerCli = require('../');
+var System = require('systemjs');
+var parse = require('recast').parse;
+var print = require('recast').print;
+var visit = require('ast-types').visit;
 
-describe('GlobalCompiler flattening', function() {
-	it('should remove the class namespace.', function() {
-		var expectedResult = fs.readFileSync('test/src/my/long/name/space/expected.js', {encoding: 'utf-8'});
-		var code = globalCompilerCli.processFile([
-			'--flatten',
-			'src/my/long/name/space/SimpleClass.js'
-		]);
+process.chdir('test');
 
-		assert.equal(code, expectedResult);
+var expected = fs.readFileSync('expected/expected.js', {encoding: 'utf-8'});
+
+System.config({
+	map: {
+		'global-compiler': 'node_modules/global-compiler/index'
+	}
+});
+
+describe('GlobalCompiler conversion', function() {
+	it('should convert namespaced code into CJS.', function(done) {
+		System.import('../src/index')
+			.then(shouldConvertNamespacedCode.bind(null, done))
+			.catch(function(error) {
+				done(error);
+			});
 	});
 });
+
+function shouldConvertNamespacedCode(done, cliModule) {
+	//Given.
+	//Clear the output dir.
+
+	//When.
+	cliModule.processFile();
+
+	//Then.
+
+	setTimeout(function() {
+		var output = fs.readFileSync('output/my/long/name/space/SimpleClass.js', {encoding: 'utf-8'});
+		assert.equal(output, expected);
+		done();
+	}, 500);
+}
