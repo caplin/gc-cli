ad3147642a9b0406351a05cfce91d220274c64ca
Brian Di Palma <offler@gmail.com>
2014-09-10 16:32:30 +0100

    Update test files to handle call expressions.

--- test/globaltocjs/expected.js
+++ test/globaltocjs/expected.js
@@ -1,8 +1,10 @@
 var SuperClass = require("my.long.name.space.SuperClass");
+var my = require("my");
+var Factory = require("my.other.name.space.Factory");
 var Field = require("my.long.name.space.Field");
 function SimpleClass() {
 	var test = new Field();
-	this.aValue = my.other.name.space.Factory.callExpression('A Literal Value');
+	this.aValue = Factory.callExpression('A Literal Value');
 }
 
 my.extend(SimpleClass, SuperClass);
