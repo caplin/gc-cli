9c6e6be26dff3d52d362b9ba1b24043e579ef152
Brian Di Palma <offler@gmail.com>
2014-09-17 17:54:32 +0100

    refactor(globaltocjs):  gather all namespaced expressions up front before transforming them

--- src/rootnstocjs.js
+++ src/rootnstocjs.js
@@ -25,24 +25,25 @@ import {isNamespacedExpressionNode} from './utils/utilities';
  */
 export var rootNamespaceVisitor = {
 	/**
-	 * @param {String[]} rootNamespaces - The root namespaces.
+	 * @param {string[]} namespaceRoots - The namespace roots, the top level part.
 	 * @param {AstNode[]} programStatements - Program body statements.
 	 * @param {string} className - The class name to export.
 	 */
-	initialize(rootNamespaces, programStatements, className) {
+	initialize(namespaceRoots, programStatements, className) {
 		this._className = className;
-		this._requiresToInsert = new Map();
-		this._rootNamespaces = rootNamespaces.map(rootNamespace => Sequence(rootNamespace));
+		this._moduleIdentifiers = new Set();
 		this._programStatements = programStatements;
+		this._namespacedNodePathsToTransform = new Map();
+		this._namespaceRoots = namespaceRoots.map(rootNamespace => Sequence(rootNamespace));
 	},
 
 	/**
 	 * @param {NodePath} identifierNodePath - Identifier NodePath.
 	 */
 	visitIdentifier(identifierNodePath) {
-		this._rootNamespaces.forEach((rootNamespace) => {
-			if (isNamespacedExpressionNode(identifierNodePath.node, rootNamespace)) {
-				replaceNamespaceWithIdentifier(identifierNodePath, this._requiresToInsert);
+		this._namespaceRoots.forEach((namespaceRoot) => {
+			if (isNodeNamespacedAndTheRootOfANamespace(identifierNodePath, namespaceRoot)) {
+				findAndStoreNodePathToTransform(identifierNodePath, this._namespacedNodePathsToTransform);
 			}
 		});
 
@@ -56,102 +57,52 @@ export var rootNamespaceVisitor = {
 		this.traverse(programNodePath);
 
 		insertExport(this._className, this._programStatements);
-		insertRequires(this._requiresToInsert, this._programStatements);
+		transform(this._namespacedNodePathsToTransform, this._moduleIdentifiers, this._programStatements);
 	}
 }
 
 /**
- * Replaces namespaced nodes with an identifier and a require statement.
- *
  * @param {NodePath} identifierNodePath - Identifier node path.
- * @param {Map} requiresToInsert - Map of requires to insert into program.
+ * @param {string} namespaceRoot - The top level of a namespace, the root label.
+ * @returns {boolean} true if this namespaced expression should be flattened.
  */
-function replaceNamespaceWithIdentifier(identifierNodePath, requiresToInsert) {
+function isNodeNamespacedAndTheRootOfANamespace(identifierNodePath, namespaceRoot) {
+	var isNodeNamespaced = isNamespacedExpressionNode(identifierNodePath.node, namespaceRoot);
+	var isRootOfExpressionTree = identifierNodePath.parent.get('object') === identifierNodePath;
+
+	return isNodeNamespaced && isRootOfExpressionTree;
+}
+
+/**
+ * @param {NodePath} identifierNodePath - Identifier node path.
+ */
+function findAndStoreNodePathToTransform(identifierNodePath, namespacedExpressionsToTransform) {
 	var nodesPath = [identifierNodePath];
 	var namespaceParts = [identifierNodePath.node.name];
 
 	populateNamespacePath(nodesPath, namespaceParts);
 
-	var parentNode = nodesPath[nodesPath.length - 1].node;
+	var namespace = namespaceParts.join('/');
+	var nodePathsToTransform = namespacedExpressionsToTransform.get(namespace) || [];
 
-	if (namedTypes.MemberExpression.check(parentNode) || namedTypes.NewExpression.check(parentNode) ||
-		namedTypes.VariableDeclarator.check(parentNode) || namedTypes.AssignmentExpression.check(parentNode)) {
-		replaceNamespacedNodeWithIdentifierAndRequire(nodesPath, namespaceParts, requiresToInsert);
-	} else if (namedTypes.CallExpression.check(parentNode)) {
-		removeMethodCalls(nodesPath, namespaceParts);
-		replaceNamespacedNodeWithIdentifierAndRequire(nodesPath, namespaceParts, requiresToInsert);
-	} else {
-		console.log('Namespaced expression not transformed to CJS, parent node type ::', parentNode.type);
-	}
+	nodePathsToTransform.push(nodesPath);
+	namespacedExpressionsToTransform.set(namespace, nodePathsToTransform);
 }
 
 /**
  * Fill provided arrays with nodes and the namespace parts that make up the namespace tree.
  *
- * @param {Array} nodesPath - Node paths that make up the namespace.
- * @param {Array} namespaceParts - Namespace parts taken from node property names.
+ * @param {NodePath[]} nodesPath - Node paths that make up the namespace.
+ * @param {string[]} namespaceParts - Namespace parts taken from node property names.
  */
 function populateNamespacePath(nodesPath, namespaceParts) {
 	var parent = nodesPath[nodesPath.length - 1].parent;
-	nodesPath.push(parent);
 
-	if (namedTypes.MemberExpression.check(parent.node) && namedTypes.Identifier.check(parent.node.property)
-	   && isIdentierNamePartOfNamespace(parent.node.property.name)) {
+	if (isAstNodePartOfNamespace(parent.node, parent.parent)) {
+		nodesPath.push(parent);
 		namespaceParts.push(parent.node.property.name);
 
 		populateNamespacePath(nodesPath, namespaceParts);
-	}
-}
-
-/**
- * Replaces namespaced node tree with single identifier and adds a require declaration for the identifier.
- *
- * @param {Array} nodesPath - Node paths that make up the namespace.
- * @param {Array} namespaceParts - Namespace parts taken from node property names.
- */
-function replaceNamespacedNodeWithIdentifierAndRequire(nodesPath, namespaceParts, requiresToInsert) {
-	var namespace = namespaceParts.join('/');
-	var requireIdentifier = namespaceParts[namespaceParts.length - 1];
-	var namespaceExpressionToReplace = nodesPath[nodesPath.length - 2];
-	var importDeclaration = createRequireDeclaration(requireIdentifier, namespace);
-
-	namespaceExpressionToReplace.replace(builders.identifier(requireIdentifier));
-	//TODO: There can be multiple import declarations with same identifier, this must be handled.
-	requiresToInsert.set(namespace, importDeclaration);
-}
-
-/**
- * Creates a CJS require declaration e.g. 'var <reqIden> = require("importedModule");'
- *
- * @param {string} requireIdentifier - The name of the identifier the require call result is set to.
- * @param {string} importedModule - The module id literal.
- */
-function createRequireDeclaration(requireIdentifier, importedModule) {
-	var requireCall = builders.callExpression(
-		builders.identifier('require'),	[
-			builders.literal(importedModule)
-		]);
-	var importDeclaration = builders.variableDeclaration('var', [
-		builders.variableDeclarator(
-			builders.identifier(requireIdentifier),
-			requireCall
-		)]);
-
-	return importDeclaration;
-}
-
-/**
- * Removes last namespace part and node path if it's a method call.
- *
- * @param {Array} nodesPath - Node paths that make up the namespace.
- * @param {Array} namespaceParts - Namespace parts taken from node property names.
- */
-function removeMethodCalls(nodesPath, namespaceParts) {
-	var namespacePart = namespaceParts[namespaceParts.length - 1];
-
-	if (namespacePart.match(/^[a-z]/)) {
-		nodesPath.pop();
-		namespaceParts.pop();
 	}
 }
 
@@ -161,24 +112,18 @@ function removeMethodCalls(nodesPath, namespaceParts) {
  * @param {string} identifierName - The identifier name to validate.
  * @returns {boolean} is the name part of a namespace.
  */
-function isIdentierNamePartOfNamespace(identifierName) {
-	if (identifierName === 'prototype' || identifierName.match(/^[A-Z_-]*$/)) {
-		return false;
+function isAstNodePartOfNamespace(astNode, parentNodePath) {
+	if (namedTypes.MemberExpression.check(astNode) && namedTypes.Identifier.check(astNode.property)) {
+		var identifierName = astNode.property.name;
+		var isPrototype = (identifierName === 'prototype');
+		var isConstant = identifierName.match(/^[A-Z_-]*$/);
+		var isMethodCall = namedTypes.CallExpression.check(parentNodePath.node)
+			&& parentNodePath.get('callee').node === astNode;
+
+		return !(isPrototype || isConstant || isMethodCall);
 	}
 
-	return true;
-}
-
-/**
- * Called after visiting ast to insert module requires.
- *
- * @param {Map} requiresToInsert - Map of requires to insert into program.
- * @param {AstNode[]} programStatements - Program body statements.
- */
-function insertRequires(requiresToInsert, programStatements) {
-	requiresToInsert.forEach((importDeclaration) => {
-		programStatements.unshift(importDeclaration);
-	});
+	return false;
 }
 
 /**
@@ -201,4 +146,61 @@ function insertExport(className, programStatements) {
 	var exportsStatement = builders.expressionStatement(assignmentExpression);
 
 	programStatements.push(exportsStatement);
+}
+
+//You need a list of variables defined in the module.
+//None of the newly created require identifiers can clash with them or with each other.
+//It's impossible to know all module variables until the entire module has been parsed.
+//This means we must keep a reference to all nodes we want to transform and build up a list of all module variables.
+//Then at the very end we must mutate all references making sure we provide unique names to all require identifiers.
+
+//We store all namespaced references we want to mutate.
+//Map <String, NodePath[]>, the String is the namespace, the NodePath is an array of references that need to be mutated.
+//Set <String> contains all the module variables.
+
+//namespacedExpressionsToTransform
+function transform(namespacedExpressionsToTransform, moduleIdentifiers, programStatements) {
+	namespacedExpressionsToTransform.forEach((nodePathsToTransform, namespace) => {
+		var requireIdentifierName = calculateModuleUniqueIdentifier(nodePathsToTransform[0]);
+		var moduleUniqueIdentifier = builders.identifier(requireIdentifierName);
+		var importDeclaration = createRequireDeclaration(requireIdentifierName, namespace);
+
+		nodePathsToTransform.forEach((nodePathToTransform) => {
+			var t = nodePathToTransform.pop();
+			t.replace(moduleUniqueIdentifier);
+		});
+
+		programStatements.unshift(importDeclaration);
+	});
+}
+
+function calculateModuleUniqueIdentifier(namespaceNodePaths) {
+	var namespacedClass = namespaceNodePaths[namespaceNodePaths.length - 1];
+
+	if (namedTypes.MemberExpression.check(namespacedClass.node)) {
+		return namespacedClass.node.property.name;
+	} else if (namedTypes.Identifier.check(namespacedClass.node)) {
+		return namespacedClass.node.name;
+	}
+
+}
+
+/**
+ * Creates a CJS require declaration e.g. 'var <reqIden> = require("importedModule");'
+ *
+ * @param {string} requireIdentifier - The name of the identifier the require call result is set to.
+ * @param {string} importedModule - The module id literal.
+ */
+function createRequireDeclaration(requireIdentifier, importedModule) {
+	var requireCall = builders.callExpression(
+		builders.identifier('require'),	[
+			builders.literal(importedModule)
+		]);
+	var importDeclaration = builders.variableDeclaration('var', [
+		builders.variableDeclarator(
+			builders.identifier(requireIdentifier),
+			requireCall
+		)]);
+
+	return importDeclaration;
 }
