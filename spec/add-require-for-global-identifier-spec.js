32956f3da1a0b7ce603912f5bf0452b1f1d0ee67
Brian Di Palma <offler@gmail.com>
2014-10-21 14:25:32 +0100

    chore(test): convert add-require-for-global-identifier test to ES6.

---
+++ spec/add-require-for-global-identifier-spec.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const assert = require('assert');
+
+const {Sequence} = require('immutable');
+const {parse, print, visit} = require('recast');
+import {addRequireForGlobalIdentifierVisitor} from '../index';
+
+const fileOptions = {encoding: 'utf-8'};
+const testResourcesLocation = 'test/add-require-for-global-identifier/';
+const givenCode = fs.readFileSync(testResourcesLocation + 'given.js', fileOptions);
+const expectedCode = fs.readFileSync(testResourcesLocation + 'expected.js', fileOptions);
+const givenAST = parse(givenCode);
+
+describe('Add require for global identifier', function() {
+	it('adds require for specified identifiers.', function() {
+		//Given.
+		var identifiersToRequire = new Map([
+			[Sequence.from(['otherGlobal']), 'otherglobal'],
+			[Sequence.from(['globalLibrary']), 'globallibrary'],
+			[Sequence.from(['aLibrary', '()', 'plugin']), 'a-library']
+		]);
+		addRequireForGlobalIdentifierVisitor.initialize(identifiersToRequire, givenAST.program.body);
+
+		//When.
+		visit(givenAST, addRequireForGlobalIdentifierVisitor);
+
+		//Then.
+		assert.equal(print(givenAST).code, expectedCode);
+	});
+});
