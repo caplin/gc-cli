c336be70f8e1e0b6e57c322a9b9e383573f8a183
Brian Di Palma <offler@gmail.com>
2014-10-21 14:00:03 +0100

    chore(test): convert globaltocjs test to ES6.

---
+++ spec/globaltocjs-spec.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const assert = require('assert');
+
+const {Sequence} = require('immutable');
+const {parse, print, visit} = require('recast');
+import {rootNamespaceVisitor} from '../index';
+
+const fileOptions = {encoding: 'utf-8'};
+const testResourcesLocation = 'test/globaltocjs/';
+const givenCode = fs.readFileSync(testResourcesLocation + 'given.js', fileOptions);
+const expectedCode = fs.readFileSync(testResourcesLocation + 'expected.js', fileOptions);
+const givenAST = parse(givenCode);
+
+describe('Global to CJS conversion', function() {
+	it('should replace globals with CJS requires.', function() {
+		//Given.
+		rootNamespaceVisitor.initialize(['my', 'other'], givenAST.program.body, 'SimpleClass');
+
+		//When.
+		visit(givenAST, rootNamespaceVisitor);
+
+		//Then.
+		assert.equal(print(givenAST).code, expectedCode);
+	});
+});
