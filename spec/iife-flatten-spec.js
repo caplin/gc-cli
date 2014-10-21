7f1bdf7a731ca0d5f4c6caae567595268900a61a
Brian Di Palma <offler@gmail.com>
2014-10-21 13:47:57 +0100

    chore(test): convert iife-flatten test to ES6.

---
+++ spec/iife-flatten-spec.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const assert = require('assert');
+
+const {Sequence} = require('immutable');
+const {parse, print, visit} = require('recast');
+import {namespacedIIFEClassVisitor} from '../index';
+
+const fileOptions = {encoding: 'utf-8'};
+const testResourcesLocation = 'test/iife-flatten/';
+const givenCode = fs.readFileSync(testResourcesLocation + 'given.js', fileOptions);
+const expectedCode = fs.readFileSync(testResourcesLocation + 'expected.js', fileOptions);
+const givenAST = parse(givenCode);
+const givenTwoLevelCode = fs.readFileSync(testResourcesLocation + 'given-twolevel.js', fileOptions);
+const expectedTwoLevelCode = fs.readFileSync(testResourcesLocation + 'expected-twolevel.js', fileOptions);
+const givenTwoLevelAST = parse(givenTwoLevelCode);
+
+describe('IIFE Namespaced class flattening', function() {
+	it('should extract class from IIFE.', function() {
+		//Given.
+		namespacedIIFEClassVisitor.initialize('my.long.name.space.SimpleClass');
+
+		//When.
+		visit(givenAST, namespacedIIFEClassVisitor);
+
+		//Then.
+		assert.equal(print(givenAST).code, expectedCode);
+	});
+
+	it('should extract class from IIFE with only two levels.', function() {
+		//Given.
+		namespacedIIFEClassVisitor.initialize('my.Class');
+
+		//When.
+		visit(givenTwoLevelAST, namespacedIIFEClassVisitor);
+
+		//Then.
+		assert.equal(print(givenTwoLevelAST).code, expectedTwoLevelCode);
+	});
+});
