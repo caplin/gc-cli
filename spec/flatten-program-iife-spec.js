8a8dfcd6fc665f7f5458ec141aaeedb45d741916
Brian Di Palma <offler@gmail.com>
2014-10-21 14:04:01 +0100

    chore(test): convert flatten-program-iife test to ES6.

---
+++ spec/flatten-program-iife-spec.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const assert = require('assert');
+
+const {Sequence} = require('immutable');
+const {parse, print, visit} = require('recast');
+import {flattenProgramIIFEVisitor} from '../index';
+
+const fileOptions = {encoding: 'utf-8'};
+const testResourcesLocation = 'test/flatten-program-iife/';
+const givenCode = fs.readFileSync(testResourcesLocation + 'given.js', fileOptions);
+const expectedCode = fs.readFileSync(testResourcesLocation + 'expected.js', fileOptions);
+const givenAST = parse(givenCode);
+
+describe('Flatten program IIFE', function() {
+	it('should flatten a program IIFE.', function() {
+		//When.
+		visit(givenAST, flattenProgramIIFEVisitor);
+
+		//Then.
+		assert.equal(print(givenAST).code, expectedCode);
+	});
+});
