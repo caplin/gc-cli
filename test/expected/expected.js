5c65abc6b2dc6d624acfc5d3ab6356c77f76aeeb
Brian Di Palma <offler@gmail.com>
2014-10-22 16:44:12 +0100

    chore(tests): remove mocha as a dependency and use jasmine for testing.

--- test/expected/expected.js
+++
@@ -1,9 +0,0 @@
-var SuperClass = require("my/long/name/space/SuperClass");
-function SimpleClass() {}
-
-my.extend(SimpleClass, SuperClass);
-
-SimpleClass.prototype.myMethod = function() {}
-
-SimpleClass.prototype.anotherMethod = function() {}
-module.exports = SimpleClass;
