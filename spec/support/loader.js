e7b7aefdc0fec7e8f31bebf9372f6059ccacb473
Brian Di Palma <offler@gmail.com>
2014-10-28 22:56:08 +0000

    refactor(loader): document spec loader and remove relative file parts

--- spec/support/loader.js
+++ spec/support/loader.js
@@ -4,8 +4,22 @@ var path = require('path');
 
 var System = require('systemjs');
 
+/**
+ * Function that loads modules needed for Jasmine tests.
+ *
+ * @param   {string} absoluteFilePath - File path with Unix path separators.
+ * @returns {Promise} Resolves when module is loaded.
+ */
 module.exports = function(absoluteFilePath) {
-	var relativeFilePath = absoluteFilePath.replace(/\.js$/, '').replace(process.cwd(), '.');
+	var cwd = process.cwd();
+
+	if (path.sep !== '/') {
+		cwd = cwd.replace(new RegExp(/\\/g), '/');
+	}
+
+	var relativeFilePath = absoluteFilePath
+			.replace(/\.js$/, '')
+			.replace(cwd, '.');
 
 	return System.import(relativeFilePath);
 }
