748230abbc1212e948350ab9e3083a3dff3bbd52
Brian Di Palma <offler@gmail.com>
2014-12-23 21:09:25 +0000

    Adding documentation to some of the matchers.

--- src/utils/matchers.js
+++ src/utils/matchers.js
@@ -13,11 +13,30 @@ export function composeMatchers(...matchers) {
 //	MATCHERS
 
 /**
+ * Creates a predicate function that checks if a NodePath is a Literal with the
+ * provided value. Will return the NodePath's parent if it matches.
  *
+ * @param   {string} value - Expected value of the literal.
+ * @returns {Function} Returns the NodePath parent if it fits search criteria.
  */
 export function literal(value) {
 	return ({node, parent}) => {
 		if (Literal.check(node) && node.value === value) {
+			return parent;
+		}
+	}
+}
+
+/**
+ * Creates a predicate function that checks if a NodePath is an Identifier with the
+ * provided name. Will return the NodePath's parent if it matches.
+ *
+ * @param   {string} name - Expected name of the identifier.
+ * @returns {Function} Returns the NodePath parent if it fits search criteria.
+ */
+export function identifier(name) {
+	return ({node, parent}) => {
+		if (Identifier.check(node) && node.name === name) {
 			return parent;
 		}
 	}
@@ -28,14 +47,6 @@ export function callExpression(callExpressionPattern) {
 		if (CallExpression.check(possibleCallExpressionNodePath.node) &&
 			callExpressionPattern.callee(possibleCallExpressionNodePath.get('callee'))) {
 			return possibleCallExpressionNodePath.parent;
-		}
-	}
-}
-
-export function identifier(name) {
-	return ({node, parent}) => {
-		if (Identifier.check(node) && node.name === name) {
-			return parent;
 		}
 	}
 }
