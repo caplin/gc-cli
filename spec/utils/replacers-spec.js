487b78f08fb5d7137e71f8a683e7973683e39ecb
Brian Di Palma <offler@gmail.com>
2014-12-31 16:36:54 +0000

    Create a transformers utils module.
    Will provide functions to transform and retrieve NodePaths.

---
+++ spec/utils/replacers-spec.js
@@ -0,0 +1,29 @@
+const assert = require('assert');
+
+//const {builders: {literal, identifier}} = require('ast-types');
+
+import {
+	parent
+} from '../../src/utils/transformers';
+
+//const variableDeclaratorTransformer = composeTransformers(
+//	literal('newlib'),
+//	parent,
+//	parent,
+//	extract({property: 'id'}),
+//	identifier('newlib')
+//);
+
+describe('transformers', function() {
+	it('parent function should return a parent NodePath when given a NodePath.', function() {
+		//Given.
+		const stubParent = {};
+		const stubNodePath = {parent: stubParent};
+
+		//When.
+		const returnedNodePath = parent(stubNodePath);
+
+		//Then.
+		assert.equal(returnedNodePath, stubParent);
+	});
+});
---
+++ src/utils/transformers.js
@@ -0,0 +1,4 @@
+
+export function parent(nodePath) {
+	return nodePath.parent;
+}
