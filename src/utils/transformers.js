a1865befdebbe8b036ae846dfc721e65f1807158
Brian Di Palma <offler@gmail.com>
2015-01-02 16:04:44 +0000

    Add extract function that extracts a property from a NodePath.

--- src/utils/transformers.js
+++ src/utils/transformers.js
@@ -2,3 +2,14 @@
 export function parent(nodePath) {
 	return nodePath.parent;
 }
+
+/**
+ * Returns a function that when provided with a NodePath will extract the
+ * requested node property.
+ *
+ * @param {string} property - Property to extract.
+ * @returns {Function} Property extractor.
+ */
+export function extract(property) {
+	return nodePath => nodePath.get(property);
+}
