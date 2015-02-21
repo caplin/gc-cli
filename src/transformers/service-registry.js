10a83994dc00e9d0f75a38bc79f0346e9f90c135
Brian Di Palma <offler@gmail.com>
2015-02-21 11:05:18 +0000

    Add a ServiceRegistry transformer.

---
+++ src/transformers/service-registry.js
@@ -0,0 +1,28 @@
+const {builders} = require('ast-types');
+
+import {
+	parent,
+	extract,
+	composeTransformers
+} from 'global-compiler/utils/transformers';
+
+const {literal, identifier} = builders;
+
+/**
+ * Creates a transformer that modifies `caplin.core.ServiceRegistry.getService` to
+ * require('service!').
+ *
+ * @param   {String}   serviceAlias Service alias to require
+ * @returns {Function} Transformer function
+ */
+export function getServiceTransformer(serviceAlias) {
+	return composeTransformers(
+		parent(),
+		parent(),
+		parent(),
+		identifier('require'),
+		parent(),
+		extract('arguments', 0),
+		literal('service!' + serviceAlias)
+	);
+}
