5977487b0fd6ac829e5bd72d906f53b60c84ac3c
Brian Di Palma <offler@gmail.com>
2015-02-28 16:16:57 +0000

    Change expected output after adding formatting options.

--- spec/resources/expected/expected.js
+++ spec/resources/expected/expected.js
@@ -3,7 +3,9 @@
 var Interface = require('my/long/Interface');
 var SuperClass = require('my/long/name/space/SuperClass');
 var topiarist = require('topiarist');
+
 function SimpleClass() {
+	this._service = require('service!myservice');
 }
 
 topiarist.extend(SimpleClass, SuperClass);
@@ -12,4 +14,5 @@ topiarist.inherit(SimpleClass, Interface);
 SimpleClass.prototype.myMethod = function() {};
 
 SimpleClass.prototype.anotherMethod = function() {};
+
 module.exports = SimpleClass;
