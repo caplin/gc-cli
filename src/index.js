b91a1f2568f00e1708b2823b7a7ee7c487c4bac0
Brian Di Palma <offler@gmail.com>
2015-03-20 22:08:25 +0000

    Allow user to specify files to compile instead of compiling an entire library.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const {Iterable} = require('immutable');
+import {List} from 'immutable';
 
 import {compileTestFiles} from './test-files-compiler';
 import {compileSourceFiles} from './src-files-compiler';
@@ -11,40 +11,44 @@ import {compileSourceFiles} from './src-files-compiler';
  * @property {boolean} compileTestFiles - True if files to compile are test files.
  * @property {Set} moduleIDsToRemove - Set of module IDs to remove following transforms.
  * @property {string[]} namespaces - Array of namespace roots to convert to CJS requires.
- * @property {Map<Iterable<string>, string>} libraryIdentifiersToRequire - Map of library identifiers to add CJS requires for.
- * @property {Set<string>} libraryIncludesToRequire - Library includes that should be transformed to requires when found.
- * @property {Iterable<string>} libraryIncludeIterable - The MemberExpression sequence that corresponds to a library include.
+ * @property {Map<List<string>, string>} libraryIdentifiersToRequire Map of library identifiers to add CJS requires for
+ * @property {Set<string>} libraryIncludesToRequire Library includes that should be transformed to requires when found
+ * @property {List<string>} libraryIncludeList - The MemberExpression sequence that corresponds to a library include.
  */
 
 /**
  * Converts CLI arguments to an OptionsObject.
  *
- * @param {Object} args - CLI arguments to configure transforms.
- * @returns {OptionsObject} An OptionsObject based on the provided CLI arguments.
+ * @param {Object} options CLI arguments to configure transforms
+ * @returns {OptionsObject} An OptionsObject based on the provided CLI arguments
  */
-export function createOptionsObject(options) {
-	var optionsObject = {
-		namespaces: options.namespaces.split(','),
-		compileTestFiles: options.compileTestFiles,
-		moduleIDsToRemove: new Set([options.removeRequires])
+export function createOptionsObject({namespaces, compileTestFiles, removeRequires, outputDirectory, _}) {
+	const optionsObject = {
+		namespaces: namespaces.split(','),
+		compileTestFiles: compileTestFiles,
+		moduleIDsToRemove: new Set([removeRequires])
 	};
 
-	if (options.compileTestFiles) {
-		optionsObject.outputDirectory = (options.outputDirectory || 'tests');
+	// _ is an array of values that aren't covered by the CLI options this can be used to provide a
+	// directory/file or list of files to convert, globs are accepted
+	optionsObject.filesToCompile = filesToCompile(_, compileTestFiles);
+
+	if (compileTestFiles) {
+		optionsObject.outputDirectory = (outputDirectory || 'tests');
 	} else {
-		optionsObject.outputDirectory = (options.outputDirectory || 'src');
+		optionsObject.outputDirectory = (outputDirectory || 'src');
 	}
 
 	optionsObject.libraryIdentifiersToRequire = new Map([
-		[Iterable(['emitr']), 'emitr'],
-		[Iterable(['jQuery']), 'jquery'],
-		[Iterable(['sinon']), 'sinonjs'],
-		[Iterable(['queryString']), 'query-string'],
-		[Iterable(['moment', '()', 'tz']), 'moment-timezone']
+		[List.of('emitr'), 'emitr'],
+		[List.of('jQuery'), 'jquery'],
+		[List.of('sinon'), 'sinonjs'],
+		[List.of('queryString'), 'query-string'],
+		[List.of('moment', '()', 'tz'), 'moment-timezone']
 	]);
 
 	optionsObject.libraryIncludesToRequire = new Set(['chosen']);
-	optionsObject.libraryIncludeIterable = Iterable(['caplin', 'thirdparty']);
+	optionsObject.libraryIncludeIterable = List.of('caplin', 'thirdparty');
 
 	return optionsObject;
 }
@@ -58,4 +62,22 @@ export function processFile(optionsObject) {
 	} else {
 		compileSourceFiles(optionsObject);
 	}
+}
+
+/**
+ * Return which files are to be compiled.
+ *
+ * @param   {Array}                cliProvidedFiles Array of files provided via the CLI
+ * @param   {Boolean}              compileTestFiles Are the test files to be compiled
+ * @returns {String|Array<String>} The files to compile
+ */
+function filesToCompile(cliProvidedFiles, compileTestFiles) {
+	// If the user provided some files via the CLI compile them
+	if (cliProvidedFiles.length > 0) {
+		return cliProvidedFiles;
+	} else if (compileTestFiles) {
+		return 'tests/**/*.js';
+	}
+
+	return 'src/**/*.js';
 }
---
+++ test/index-spec.js
@@ -0,0 +1,76 @@
+import {
+	equal,
+	deepEqual
+} from 'assert';
+
+import {describe, it} from 'mocha';
+
+import {createOptionsObject} from '../src/index';
+
+describe('index', () => {
+	it('should create a default options object with default arguments options.', () => {
+		// Given.
+		const stubDefaultArgumentsObject = {
+			_: [],
+			compileTestFiles: false,
+			t: false,
+			removeRequires: 'caplin',
+			r: 'caplin',
+			namespaces: 'caplin,caplinx,caplinps,ct,br',
+			n: 'caplin,caplinx,caplinps,ct,br'
+		};
+
+		// When.
+		const optionsObject = createOptionsObject(stubDefaultArgumentsObject);
+
+		// Then.
+		equal(optionsObject.outputDirectory, 'src');
+		equal(optionsObject.compileTestFiles, false);
+		equal(optionsObject.filesToCompile, 'src/**/*.js');
+		deepEqual(optionsObject.namespaces, ['caplin', 'caplinx', 'caplinps', 'ct', 'br']);
+	});
+
+	it('should create an options object with test arguments options.', () => {
+		// Given.
+		const stubDefaultArgumentsObject = {
+			_: [],
+			compileTestFiles: true,
+			t: true,
+			removeRequires: 'caplin',
+			r: 'caplin',
+			namespaces: 'caplin,caplinx,caplinps,ct,br',
+			n: 'caplin,caplinx,caplinps,ct,br'
+		};
+
+		// When.
+		const optionsObject = createOptionsObject(stubDefaultArgumentsObject);
+
+		// Then.
+		equal(optionsObject.outputDirectory, 'tests');
+		equal(optionsObject.compileTestFiles, true);
+		equal(optionsObject.filesToCompile, 'tests/**/*.js');
+		deepEqual(optionsObject.namespaces, ['caplin', 'caplinx', 'caplinps', 'ct', 'br']);
+	});
+
+	it('should create an options object with provided files to compile.', () => {
+		// Given.
+		const stubDefaultArgumentsObject = {
+			_: ['src/caplin/core/event/PopoutAwareEventHub.js'],
+			compileTestFiles: false,
+			t: false,
+			removeRequires: 'caplin',
+			r: 'caplin',
+			namespaces: 'caplin,caplinx,caplinps,ct,br',
+			n: 'caplin,caplinx,caplinps,ct,br'
+		};
+
+		// When.
+		const optionsObject = createOptionsObject(stubDefaultArgumentsObject);
+
+		// Then.
+		equal(optionsObject.outputDirectory, 'src');
+		equal(optionsObject.compileTestFiles, false);
+		equal(optionsObject.filesToCompile, 'src/caplin/core/event/PopoutAwareEventHub.js');
+		deepEqual(optionsObject.namespaces, ['caplin', 'caplinx', 'caplinps', 'ct', 'br']);
+	});
+});
