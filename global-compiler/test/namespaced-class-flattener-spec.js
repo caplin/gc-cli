c06cf6b67666e3baa7dec5e411bdf1fb476278b4
briandipalma <offler@gmail.com>
2016-05-10 17:11:23 +0100

    refactor(namespaced-class-flattener): Update UT to latest style

--- global-compiler/test/namespaced-class-flattener-spec.js
+++ global-compiler/test/namespaced-class-flattener-spec.js
@@ -1,56 +1,46 @@
-import {equal} from 'assert';
-import {readFileSync} from 'fs';
-
-import {parse, print, visit} from 'recast';
+import {describe, it} from 'mocha';
+import {visit} from 'recast';
+import {getAST, verifyASTIsAsExpected} from './test-utilities';
 
 import {namespacedClassFlattenerVisitor} from '../src/index';
-
-const fileOptions = {encoding: 'utf-8'};
-const testResourcesLocation = 'test/resources/namespaced-class-flattener/';
-
-const givenCode = readFileSync(testResourcesLocation + 'given.js', fileOptions);
-const expectedCode = readFileSync(testResourcesLocation + 'expected.js', fileOptions);
-const givenAST = parse(givenCode);
-
-const givenObjectCode = readFileSync(testResourcesLocation + 'given-object.js', fileOptions);
-const expectedObjectCode = readFileSync(testResourcesLocation + 'expected-object.js', fileOptions);
-const givenObjectAST = parse(givenObjectCode);
-
-const givenTwoLevelObjectCode = readFileSync(testResourcesLocation + 'given-twolevel.js', fileOptions);
-const expectedTwoLevelObjectCode = readFileSync(testResourcesLocation + 'expected-twolevel.js', fileOptions);
-const givenTwoLevelObjectAST = parse(givenTwoLevelObjectCode);
 
 describe('Namespaced class flattening', () => {
 	it('should remove the class namespace.', () => {
 		// Given
+		const givenAST = getAST('namespaced-class-flattener', 'given');
+
 		namespacedClassFlattenerVisitor.initialize('my.long.name.space.SimpleClass');
 
 		// When
 		visit(givenAST, namespacedClassFlattenerVisitor);
 
 		// Then
-		equal(print(givenAST).code, expectedCode);
+		verifyASTIsAsExpected('namespaced-class-flattener', 'expected', givenAST);
 	});
 
 	it('should remove object namespacing.', () => {
 		// Given
+		const givenObjectAST = getAST('namespaced-class-flattener', 'given-object');
+
 		namespacedClassFlattenerVisitor.initialize('my.long.name.space.SimpleObject');
 
 		// When
 		visit(givenObjectAST, namespacedClassFlattenerVisitor);
 
 		// Then
-		equal(print(givenObjectAST).code, expectedObjectCode);
+		verifyASTIsAsExpected('namespaced-class-flattener', 'expected-object', givenObjectAST);
 	});
 
 	it('should remove two level object namespacing.', () => {
 		// Given
+		const givenTwoLevelObjectAST = getAST('namespaced-class-flattener', 'given-twolevel');
+
 		namespacedClassFlattenerVisitor.initialize('my.SimpleUtilityObject');
 
 		// When
 		visit(givenTwoLevelObjectAST, namespacedClassFlattenerVisitor);
 
 		// Then
-		equal(print(givenTwoLevelObjectAST).code, expectedTwoLevelObjectCode);
+		verifyASTIsAsExpected('namespaced-class-flattener', 'expected-twolevel', givenTwoLevelObjectAST);
 	});
 });
