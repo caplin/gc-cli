2d53501ab9e12b929873f3567fcddda69dbc70c8
briandipalma <offler@gmail.com>
2016-05-11 10:29:07 +0100

    refactor(var-namespace-alias-expander): Update UT style

--- global-compiler/test/var-namespace-alias-expander-spec.js
+++ global-compiler/test/var-namespace-alias-expander-spec.js
@@ -1,24 +1,20 @@
-const fs = require('fs');
-const assert = require('assert');
+import {describe, it} from 'mocha';
+import {visit} from 'recast';
 
-const {parse, print, visit} = require('recast');
 import {varNamespaceAliasExpanderVisitor} from '../src/index';
-
-const fileOptions = {encoding: 'utf-8'};
-const testResourcesLocation = 'test/resources/var-namespace-alias-expander/';
-const givenCode = fs.readFileSync(testResourcesLocation + 'given.js', fileOptions);
-const expectedCode = fs.readFileSync(testResourcesLocation + 'expected.js', fileOptions);
-const givenAST = parse(givenCode);
+import {getAST, verifyASTIsAsExpected} from './test-utilities';
 
 describe('var namespace alias expander', () => {
 	it('should expand var namespace aliases to namespaces.', () => {
-		//Given.
+		// Given.
+		const givenAST = getAST('var-namespace-alias-expander', 'given');
+
 		varNamespaceAliasExpanderVisitor.initialize(['my']);
 
-		//When.
+		// When.
 		visit(givenAST, varNamespaceAliasExpanderVisitor);
 
-		//Then.
-		assert.equal(print(givenAST).code, expectedCode);
+		// Then.
+		verifyASTIsAsExpected('var-namespace-alias-expander', 'expected', givenAST);
 	});
 });
