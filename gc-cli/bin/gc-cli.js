24af7103c907ec0cc1955a0825a8c233e7d872fa
briandipalma <offler@gmail.com>
2016-05-27 11:49:40 +0100

    feat: Log error if the version of node is not supported

--- gc-cli/bin/gc-cli.js
+++ gc-cli/bin/gc-cli.js
@@ -1,30 +1,58 @@
 #!/usr/bin/env node
 
-/* eslint-disable id-length, strict */
-'use strict';
+/* eslint-disable id-length, no-var */
+'use strict'; // eslint-disable-line
+
+var path = require('path');
+
+var check = require('check-node-version');
+var parseArgs = require('minimist');
+var error = require('winston').error;
+
+var packageJson = require(path.join(__dirname, '..', '..', 'package.json'));
+
+function versionCheckCallback(versionError, result) {
+	var errorMessage = 'Not compatible with current node version, please update!';
+
+	if (result.nodeSatisfied) {
+		compileFiles();
+	} else {
+		if (versionError) {
+			errorMessage = versionError.message;
+		}
+
+		error(errorMessage);
+		process.exit(1); // eslint-disable-line
+	}
+}
+
+var options = {
+	node: packageJson.engines.node
+};
+
+check(options, versionCheckCallback);
 
 require('babel-register')({
 	ignore: false
 });
-const parseArgs = require('minimist');
 
-const gcCli = require('../src/index');
+function compileFiles() {
+	var commandParseOptions = {
+		alias: {
+			n: 'namespaces',
+			r: 'removeRequires',
+			o: 'outputDirectory',
+			t: 'compileTestFiles'
+		},
+		default: {
+			compileTestFiles: false,
+			removeRequires: 'caplin',
+			namespaces: 'caplin,caplinx,caplinps,ct,br'
+		}
+	};
+	var gcCli = require('../src/index'); // eslint-disable-line
+	var options = parseArgs(process.argv.slice(2), commandParseOptions);
+	var optionsObject = gcCli.createOptionsObject(options);
 
-const commandParseOptions = {
-	alias: {
-		n: 'namespaces',
-		r: 'removeRequires',
-		o: 'outputDirectory',
-		t: 'compileTestFiles'
-	},
-	default: {
-		compileTestFiles: false,
-		removeRequires: 'caplin',
-		namespaces: 'caplin,caplinx,caplinps,ct,br'
-	}
-};
-
-const options = parseArgs(process.argv.slice(2), commandParseOptions);
-const optionsObject = gcCli.createOptionsObject(options);
-
-gcCli.processFile(optionsObject);
+	gcCli.processFile(optionsObject);
+}
